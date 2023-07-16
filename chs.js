291b6b98d79cee9283a8e8af0e704c35ba78ed02
guenislee <69913308+guenislee@users.noreply.github.com>
2023-07-16 15:11:56 +0800

    Update chs.js

--- chs.js
+++ chs.js
@@ -985,7 +985,7 @@ var cnItems = {
     "Research": "研究",
     "Game Data": "游戏数据",
     "Export to file": "导出到文件",
-    "Export": "导出字符串",
+    "Export": "导出到文件",
     "Import from file": "从文件导入",
     "UTF-8": "UTF-8",
     "Error importing file": "导入文件错误",
@@ -1406,6 +1406,8 @@ var cnRegReplace = new Map([
     [/^Req: (.+) \/ (.+) elves$/, '要求：$1 \/ $2 精灵'],
     [/^Usages: (\d+)\/$/, '用途：$1\/'],
     [/^workers: (\d+)\/$/, '工人：$1\/'],
+    [/^Happiness: (.+)$/, '幸福度：$1'],
+    
     /*
     [/^Building: (.+)$/, '建筑：$1'],
     [/^Upgrade: (.+)$/, '升级：$1'],
