0dd09bdd66092a1d58fe5c9e74f2676cf6f7e8d0
Moonwalk <69913308+guenislee@users.noreply.github.com>
2023-10-16 10:23:05 +0800

    Update chs.js

--- chs.js
+++ chs.js
@@ -1589,7 +1589,7 @@ var cnItems = {
     "Technology Market": "科技市场",
     "You may miss something important about your civilization development. But maybe there was someone who faced same problems and ready to share his knowledge with you? Unlock xp upgrades providing access to new buildings and upgrades.": "您可能遗失了一些对于发展至关重要的科技，但或许有其他人遇到了类似的发展瓶颈，能不能借鉴一下他们的解决办法呢？解锁一类全新的经验升级，它们将解锁新的建筑和升级，",
     "Auto-upgrade": "自动升级",
-    "You can enable bot that automatically doing upgrades for you. Level 1 upgrades up to 100 research cost, but each level multiplies this limit by 4 (Need to be turned on in settings)": "您可以使用机器人来自动帮您完成升级。1 级可以自动升级 400 研究上限以内的研究，每级使该上限变为上一级的 4 倍（需要在设置中启用）",
+    "You can enable bot that automatically doing upgrades for you. Level 1 upgrades up to 400 research cost, but each level multiplies this limit by 4 (Need to be turned on in settings)": "您可以使用机器人来自动帮您完成升级。1 级可以自动升级 400 研究上限以内的研究，每级使该上限变为上一级的 4 倍（需要在设置中启用）",
     "Planet Memory": "星球记忆",
     "Why dont spend some space on our datacenters hard drive to remember our favorite planet? Unlock option to remember one planet upon search.": "何不在数据中心的硬盘上划出一块区域来存储您最喜欢的行星呢？解锁固定已找到行星的选项",
     "Relics": "遗物",
